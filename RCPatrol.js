1fe6dfe0b5eed05bb482d658a9916e31376e82a3
Xi-Plus <huangxuanyuxiplus@gmail.com>
2022-06-02 13:53:00 +0800

    Add more classes

--- RCPatrol.js
+++ RCPatrol.js
@@ -15,13 +15,20 @@
 			'rctitle': mw.config.get('wgPageName'),
 		}).then(function(data) {
 			var recentchanges = data.query.recentchanges;
+			var found_patrolled = false;
 			for (var i = 0; i < recentchanges.length; i++) {
-				console.log(recentchanges[i]);
-				if (recentchanges[i].patrolled !== undefined) {
-					break;
+				if (recentchanges[i].autopatrolled !== undefined) {
+					$('.mw-contributions-list li[data-mw-revid=' + recentchanges[i].revid + ']').addClass('gadget-rcp-autopatrolled');
+					found_patrolled = true;
+				} else if (recentchanges[i].patrolled !== undefined) {
+					$('.mw-contributions-list li[data-mw-revid=' + recentchanges[i].revid + ']').addClass('gadget-rcp-patrolled');
+					found_patrolled = true;
+				} else {
+					$('.mw-contributions-list li[data-mw-revid=' + recentchanges[i].revid + ']').addClass('gadget-rcp-unpatrolled');
+					if (!found_patrolled) {
+						$('.mw-contributions-list li[data-mw-revid=' + recentchanges[i].revid + ']').addClass('gadget-rcp-recently-unpatrolled');
+					}
 				}
-				console.log($('.mw-contributions-list li[data-mw-revid=' + recentchanges[i].revid + ']').length);
-				$('.mw-contributions-list li[data-mw-revid=' + recentchanges[i].revid + ']').addClass('gadget-rcp-not-patrolled');
 			}
 		});
 	}
